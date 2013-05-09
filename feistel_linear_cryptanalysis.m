d2a9bb5594489a68a4dec7accab306003591fd81
Stefano Zanella <zanella.stefano@gmail.com>
2013-05-09 18:39:15 +0200

    Start working on linear cryptanalysis.
    Add provided KPA data.
    Start implementing linear cryptanalysis script: key multiplication matrix looks
    non-invertible.

---
+++ feistel_linear_cryptanalysis.m
@@ -0,0 +1,34 @@
+% required by Octave - otherwise hex2bi and bi2hex don't work
+pkg load communications
+
+
+%filename = 'KPAlinear.hex';
+%kpa_pairs = textscan(fopen(filename), '%s  %s');
+
+
+lu = 32;
+pulse_input = pulse_key = '00000001';
+
+disp('Cycling the plaintext');
+key = '00000000';
+
+B = []
+for shift = 1:lu
+  plaintext = bi2hex(circshift(hex2bi(pulse_input), [0, -(shift-1)]));
+
+  b_i = hex2bi(feistel_encrypt(plaintext, key, 4, @linear_round_function, @half_outward_shift));
+  B = [B; b_i];
+end
+disp (B);
+
+disp('Cycling the key');
+plaintext = '00000000';
+
+A = []
+for shift = 1:lu
+  key = bi2hex(circshift(hex2bi(pulse_key), [0, -(shift-1)]));
+
+  a_i = hex2bi(feistel_decrypt(plaintext, key, 4, @linear_round_function, @half_outward_shift));
+  A = [A; a_i];
+end
+disp(A);
